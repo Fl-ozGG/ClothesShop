68c8f6c6f91795f059d28546d3751337a7a36e26
Fl-ozGG <gurreaignasi@gmail.com>
2025-10-22 09:11:49 +0200

    tests definidos en base a los requerimientos iniciales (proyecto TDD + DDD)

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿namespace TestProject1;
+
+public class UnitTest1
+{
+   [Fact]
+    public void SellProduct_ReturnsFalse_IfThereIsNotEnoughStock()
+    {
+        
+    }
+
+    [Fact]
+    public void SellProduct_ShouldReduce_AmountOfProduct()
+    {
+        
+    }
+
+    [Fact]
+    public void AddProduct_ReturnsFalse_IfIsNotAssociatedToSupplier()
+    {
+            
+    }
+    
+    [Fact]
+    public void UserRegister_ShouldNotAllow_AnEmailAlreadyRegistered()
+    {
+        
+    }
+
+    [Fact]
+    public void RemoveProduct_CantEliminate_IfThereIsSalesRelatedWithTheProduct()
+    {
+        
+    }
+
+    [Fact]
+    public void RemoveProduct_CantEliminate_IfThereIsNoProduct()
+    {
+        
+    }
+    
+}
